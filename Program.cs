949b0d9bebc845efb493bb2ea79bd486736a8428
Túlio Albuquerque <tulioalbuquerque@outlook.com>
2024-09-26 19:35:09 -0300

    fix: removes todo comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
 nokia.Ligar();
